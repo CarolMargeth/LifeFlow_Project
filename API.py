8cb659e7936d9593f5fc47462cf1aa4ed1df8a68
Carol Calderon <cmcalderonr@unal.edu.co>
2024-01-29 22:34:20 -0500

    DB connection with psycopg

--- API.py
+++ API.py
@@ -2,14 +2,13 @@
 
 from fastapi import FastAPI
 from dto.user import User
-from psycopgPOST import insertUser
-    
+from services.users import insertUser
+
 app = FastAPI()
 
 @app.post("/users/")
 async def CreateUser(user: User):
     insertUser(user)
-    # print(user)
     return user
 
 
